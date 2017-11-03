577dac88857e0599d2335ac657bea6ac944219ec
twu54 <twu54@illinois.edu>
2017-11-02 22:50:22 -0500

    Fix message file_name parameter problem

--- mp3/message.py
+++ mp3/message.py
@@ -14,6 +14,7 @@ def decoded(message):
 	return json.loads(message)
 
 
+
 def return_all_received(sock, length):
 	received = 'enter_loop'
 	result = ""
@@ -31,19 +32,28 @@ def receive_all_decrypted(sock):
 	return decoded(return_all_received(sock, get_length(sock)))
 
 # send the first complete message instead of return
-def receive_all_to_target(sock, target):
+# an optional argument target is included for sending to another destination (relaying)
+def receive_all_to_target(sock, target = None):
+	file_name = receive_all_decrypted(sock)
 	len_left_over = get_length(sock)
 	if type(target) == socket._socketobject:
+		send_all_encrypted(file_name)
 		target.sendall(str(len_left_over).zfill(BYTES_INT))
+	else: # don't replay, send to file
+		target = open(file_name, 'w')
 	received = 'enter_loop'
 
 	while (len(received) > 0 and len_left_over > 0):
 		received = sock.recv(min(len_left_over, SIZE))
-		if type(target) ==  socket._socketobject:
+		if type(target) == socket._socketobject:
 			target.sendall(received)
 		else: # file 
 			target.write(received)
-		
+
+	if type(target) != socket._socketobject:
+		target.close()
+
+
 
 def fill_header(message_length):
 	length_str = str(message_length)		
@@ -56,13 +66,15 @@ def fill_header(message_length):
 def send_all_encrypted(sock, message):
 	encoded_m = encoded(message)
 	header = fill_header(len(encoded_m))
-	sock.sendall(header)
+	sock.sendall(header) 
 	sock.sendall(encoded_m)
 
 
 # send the whole file content piece by piece without encryption
-def send_all_from_file(sock, file_obj):
-	header = fill_header(os.fstat(file_obj.fileno()).st_size)
+def send_all_from_file(sock, file_name):
+	send_all_encrypted(sock, file_name)
+	with open(file_name,'r') as f:
+		file_content = f.read()
+	header = fill_header(len(file_content))
 	sock.sendall(header)
-	for line in file_obj:
-		sock.sendall(line)
+	sock.sendall(file_content)
