a76899fa9d46f6ea872472330914f450acae6dc8
twu54 <twu54@illinois.edu>
2017-11-16 00:09:57 -0600

    Support parallel install pip

---
+++ scripts/pip_install.sh
@@ -0,0 +1,15 @@
+
+read    -p "Username: " username
+read -s -p "Password: " password && echo 
+
+> hostnames.txt
+for x in {1..10} 
+do
+	while [[ ${#x} -lt 2 ]] ; do
+		x="0${x}"
+	done
+	echo "${username}@fa17-cs425-g48-${x}.cs.illinois.edu" >> hostnames.txt
+done
+
+sshpass -p $password pssh -h hostnames.txt -A -x '-tt'  -i "-O StrictHostKeyChecking=no" -l root \
+"echo ${password} | sudo -S pip install ${1}"
